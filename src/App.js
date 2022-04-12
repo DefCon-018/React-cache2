edfe7abb219ae9a4e3333599d95f323374e95d64
Himanshu Pandey <hp761570@gmail.com>
2022-04-12 11:21:13 +0530

    Initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+import { useClearCache } from "react-clear-cache";
+
+const App: React.FC<{}> = () => {
+  const { isLatestVersion, emptyCacheStorage } = useClearCache();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      {!isLatestVersion && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              emptyCacheStorage();
+            }}
+          >
+            Update version
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
-}
+};
 
 export default App;
